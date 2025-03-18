b1c55b7b903fd9ef2a189329ffc465b1eb5b78de
Simon Nestler <simon.nestler@thi.de>
2025-03-18 13:51:26 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Softwareentwicklung se = new Softwareentwicklung("Max Mustermann");
+        se.setDozent("Eva Musterfrau");
+    }
+}
---
+++ src/Softwareentwicklung.java
@@ -0,0 +1,27 @@
+public class Softwareentwicklung {
+
+    private String dozent;
+
+    public Softwareentwicklung(String dozent) {
+        this.dozent = dozent;
+        System.out.println("Ihr Dozent: " + this.dozent);
+    }
+
+    public String getDozent() {
+        return this.dozent;
+    }
+
+    public void setDozent(String dozent) {
+        this.dozent = dozent;
+    }
+
+    public boolean lernen(int dauer) {
+        if (dauer < 15) {
+            System.out.println(dauer + " Minuten sind zu kurz zum Lernen!!!");
+            return false;
+        } else {
+            System.out.println("Lernen für " + dauer + " Minuten");
+            return true;
+        }
+    }
+}
